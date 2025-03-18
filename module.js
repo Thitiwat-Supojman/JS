72a5e6bc7cd63fb8cef5f688918ad57d0f4a318c
THITIWAT <thitiwatsupojman@gmail.com>
2025-03-18 19:14:48 +0700

    Final  commit

---
+++ module.js
@@ -0,0 +1,10 @@
+// module.js
+export function add(a, b) {
+    return a + b;
+}
+
+export function greet() {
+    return "Hello World";
+}
+
+export const name = "Thitiwat Supojman";
---
+++ script.js
@@ -0,0 +1,5 @@
+import { add, greet, name } from './module.js';
+
+console.log(add(5, 5)); // 10
+console.log(greet());   // Hello World
+console.log(name);      // Thitiwat Supojman
